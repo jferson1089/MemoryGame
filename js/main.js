d882eb6d3ab7f9f75d4ba4f41f0c24938baa2ef9
Mic Doodle <mcdoodle@Mics-MBP.fios-router.home>
2020-01-20 23:53:00 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,24 @@
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
+const cards = ["queen", "queen", "king", "king"];
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+const cardsInPlay = [];
+
+const cardOne = cards[0];
+
+cardsInPlay.push(cardOne);
+
+console.log("User flipped Queen!");
+
+const cardTwo = cards[1];
+
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped King!");
+
+if(cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]){
+		alert("You found a match!");
+	
+	}else{
+		alert("Sorry try again!");
+	}
+	}
